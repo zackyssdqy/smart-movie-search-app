dede5c1a9746107066e252f5314109d42eebffe1
zackyssdqy <zaassetengah@gmail.com>
2026-06-09 23:16:11 +0700

    feat: add movie card and search bar component

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <article class="overflow-hidden rounded-xl bg-slate-900 shadow-lg transition hover:-translate-y-1 hover:bg-slate-800">
+    <RouterLink :to="`/movie/${movie.imdbID}`" class="block">
+      <img :src="getPoster(movie.Poster)" :alt="movie.Title" class="h-80 w-full object-cover" />
+
+      <div class="p-4">
+        <h2 class="line-clamp-2 font-semibold text-white">
+          {{ movie.Title }}
+        </h2>
+
+        <p class="mt-1 text-sm text-slate-400">
+          {{ movie.Year }} • {{ movie.Type }}
+        </p>
+      </div>
+    </RouterLink>
+  </article>
+</template>
+
+<script setup>
+defineProps({
+  movie: {
+    type: Object,
+    required: true,
+  },
+})
+
+const getPoster = (poster) => {
+  if (!poster || poster === 'N/A') {
+    return 'https://placehold.co/300x445?text=No+Poster'
+  }
+
+  return poster
+}
+</script>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <input type="text" :value="modelValue" placeholder="Search movie title..."
+    class="w-full rounded-xl border border-slate-700 bg-slate-900 px-4 py-3 text-white outline-none placeholder:text-slate-500 focus:border-blue-500"
+    @input="$emit('update:modelValue', $event.target.value)" />
+
+  <!-- <button type="submit" :disabled="loading || !modelValue.trim()"
+      class="rounded-xl bg-blue-600 px-6 py-3 font-semibold text-white hover:bg-blue-700 disabled:cursor-not-allowed disabled:bg-slate-700">
+      {{ loading ? 'searching' : 'search' }}
+    </button> -->
+
+</template>
+<script setup>
+defineProps({
+  modelValue: {
+    type: String,
+    required: true,
+  },
+  loading: {
+    type: Boolean,
+    default: false,
+  }
+})
+
+// const emit = defineEmits(['update:modelValue', 'search'])
+
+</script>
